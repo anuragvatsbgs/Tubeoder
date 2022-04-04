efc7e3e3227ddadc58d900e04d63ae734403fe17
ANURAG KUMAR <anuragvatsbgs@gmail.com>
2022-04-04 23:11:20 +0530

    final tube for mobile device

--- main.py
+++ main.py
@@ -13,21 +13,27 @@ from kivy.storage.jsonstore import JsonStore
 import requests
 import os
 import time
+from kivy.utils import platform
 
 
-#Window.size = (400, 520)
+if platform == "android":
+    from android.permissions import request_permissions, Permission
+    request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
+
+
+
+
 
 class TubeOder(MDApp):
     global screen_manager
     screen_manager = ScreenManager()
 
     def build(self):
-        kv = Builder.load_file("welcomescreen.kv")
         kv1 = Builder.load_file("main.kv")
         kv2 = Builder.load_file("conv.kv")
         kv3 = Builder.load_file("final.kv")
         self.theme_cls.primary_palette = "Green"
-        screen_manager.add_widget(kv)
+      
         screen_manager.add_widget(kv1)
         screen_manager.add_widget(kv2)
         screen_manager.add_widget(kv3)
@@ -42,11 +48,6 @@ class TubeOder(MDApp):
         except (requests.ConnectionError, requests.Timeout) as exception:
             self.alpha=0
         
-            
-        
-        Clock.schedule_once(self.change_screen, 10)
-
-    def change_screen(self, dt):
         if self.alpha==0:
             cancel_btn_username_dialogue = MDFlatButton(text='Cancel',on_release = self.close_username_dialogue)
             self.dialog = MDDialog(title = 'Error',text = "Please check your internet connection",size_hint = (0.7,0.2),buttons = [cancel_btn_username_dialogue])
@@ -61,33 +62,77 @@ class TubeOder(MDApp):
             self.dialog = MDDialog(title = 'Error',text = "Please input a valid Link",size_hint = (0.7,0.2),buttons = [cancel_btn_username_dialogue])
             self.dialog.open()
         else:
-            youtube_1=YouTube(link)
-            screen_manager.get_screen('MainScreen').ids.imagexpa.source=youtube_1.thumbnail_url
-            screen_manager.get_screen('MainScreen').ids.titlexpa.text=youtube_1.title
+            self.youtube_1=YouTube(link)
+            screen_manager.get_screen('MainScreen').ids.imagexpa.source=self.youtube_1.thumbnail_url
+            screen_manager.get_screen('MainScreen').ids.titlexpa.text=self.youtube_1.title
             screen_manager.get_screen('MainScreen').ids.disabled_button.disabled = False
-            self.videos=youtube_1.streams
+            self.videos=self.youtube_1.streams
             
             for i in self.videos:
-                screen_manager.get_screen('Conv').ids.container.add_widget(OneLineListItem(text=f"{i}", on_release=self.presser))
+                
+                alp=str(i).split()
+               
+                tagg=alp[1]
+                tagg1=tagg.split('"')
+                a=tagg1[1]
+                b=int(a)
+                
+                
+                
+                tpe=alp[2]
+                tagg2=tpe.split('"')
+                a1=tagg2[1]
+                
+                
+                res=alp[3]
+                tagg3=res.split('"')
+                a2=tagg3[1]
+                
+                
+                fpss=alp[4]
+                tagg4=fpss.split('"')
+                a3=tagg4[1]
+                
+                
+                f1=b,a1,a2,a3
+                
+                screen_manager.get_screen('Conv').ids.container.add_widget(OneLineListItem(text=f"{f1}", on_release=self.presser))
             
                 
 
     def close_username_dialogue(self,obj):
         self.dialog.dismiss()
         
+
+        
     def presser(self, onelinelistitem):
         sef=self.videos
         vid=list(enumerate(self.videos))
         sef1=onelinelistitem.text
-        length=len(sef)
-        for i in range(length):
-            if str(sef[i])==str(sef1):
-                self.sto=sef[i]
-                home = os.path.expanduser('~')
-                download_path = os.path.join(home, 'Tubeoder')
-                self.sto.download(download_path)
-                screen_manager.get_screen('FinalScreen').ids.sucess.text="Sucessfull!"
-                screen_manager.get_screen('FinalScreen').ids.sucess1.text=download_path
        
+        length=len(sef)
+        
+        alp11=sef1.split()
+        
+        fpss1=alp11[0]
+        alp12=fpss1.split('(')
+        alp13=alp12[1]
+        alp14=alp13.split(',')
+        alp15=alp14[0]
+        matag=int(alp15)
+        
+        
+        if platform=="android":
+            download_path="/storage/emulated/0/Tubeoder"
+        else:
+            home = os.path.expanduser('~')
+            download_path = os.path.join(home, 'Tubeoder')
+            
+        strmm=self.youtube_1.streams.get_by_itag(matag)
+        strmm.download(download_path)
+        screen_manager.get_screen('FinalScreen').ids.sucess.text="Sucessfull!"
+        screen_manager.get_screen('FinalScreen').ids.sucess1.text=download_path     
         screen_manager.current="FinalScreen"
+        
+       
 TubeOder().run()
